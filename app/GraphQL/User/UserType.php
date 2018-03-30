3068d636179e781604ed9d642689b0d96f1db18b
Bruno Henry <brunohenryt@hotmail.com>
2018-03-29 21:28:42 -0300

    graphql installed and tested

---
+++ app/GraphQL/User/UserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\GraphQL\User;
+
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Type as GraphQLType;
+
+class UserType extends GraphQLType
+{
+    protected $attributes = [
+        'name' => 'User',
+        'description' => 'A user'
+    ];
+
+    public function fields()
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'The id of the user'
+            ],
+            'email' => [
+                'type' => Type::string(),
+                'description' => 'The email of user'
+            ]
+        ];
+    }
+
+    protected function resolveEmailField($root, $args)
+    {
+        return strtolower($root->email);
+    }
+}
